5b2857923cef3c12f8d30797f21cc05842900e90
annmcalvert <annmcalvert@gmail.com>
2019-08-05 23:03:17 -0500

    finished

--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
-let app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello from the web server side...');
+});
+
+app.post('/formsubmissions', (req, res) => {
+    const data = {
+        name: req.body.name,
+        email: req.body.email
+    };
+    fs.appendFileSync('formdata.txt', JSON.stringify(data));
+    res.json(req.body);
 });
 
 app.listen(3000);
